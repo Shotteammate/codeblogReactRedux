d19ff6962991d5579cd10f2c95116821938ca0a7
Tik San Pun <shotteammate@gmail.com>
2019-10-07 16:05:05 +0800

    Build Navbar Component

---
+++ src/components/layout/SignInLink.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+
+const SignInLink = () => {
+  return (
+    <ul className="right">
+      <li><NavLink to='/'>Add Post</NavLink></li>
+      <li><NavLink to='/'>Log Out</NavLink></li>
+      <li><NavLink to='/' className='btn btn-floating blue darken-2'>temp</NavLink></li>
+    </ul>
+  )
+}
+
+export default SignInLink;
