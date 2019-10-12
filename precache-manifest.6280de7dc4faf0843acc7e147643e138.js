1afb772906e4f7bbfc15c7af702e8db6bd908a2b
Tik San Pun <shotteammate@gmail.com>
2019-10-12 15:57:17 +0800

    Updates

---
+++ precache-manifest.6280de7dc4faf0843acc7e147643e138.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5b8fd51a9b14313464363b24fcfca8",
+    "url": "/codeblogRedux/index.html"
+  },
+  {
+    "revision": "cfbf0bdb1937e1da72d9",
+    "url": "/codeblogRedux/static/css/main.a053e6cc.chunk.css"
+  },
+  {
+    "revision": "f41d9d0d930f2f5f2a4f",
+    "url": "/codeblogRedux/static/js/2.1eb6e9af.chunk.js"
+  },
+  {
+    "revision": "cfbf0bdb1937e1da72d9",
+    "url": "/codeblogRedux/static/js/main.8ace90cb.chunk.js"
+  },
+  {
+    "revision": "5d1d0cab634b5edcbc34",
+    "url": "/codeblogRedux/static/js/runtime-main.e52d642a.js"
+  }
+]);
